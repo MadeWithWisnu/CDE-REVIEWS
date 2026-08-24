ad2d76daa01ee23e7483d834ef4c15ee2c89f721
MadeWithWisnu <made_wisnu@mitsuilease.co.id>
2026-08-24 09:11:51 +0700

    add all component

--- doc-viewer-arch/Confins.Gateway.Api/obj/Debug/net8.0/Confins.Gateway.Api.AssemblyInfo.cs
+++ doc-viewer-arch/Confins.Gateway.Api/obj/Debug/net8.0/Confins.Gateway.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Confins.Gateway.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62207c6663d66ac14435888d5993bc09835b5d86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Confins.Gateway.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Confins.Gateway.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
